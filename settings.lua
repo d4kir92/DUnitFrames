ab536176f79dacf9e9477bc99ce9cbd607b8c884
D4KiR <d4kir92@gmail.com>
2022-11-21 08:32:12 +0100

    v1.2.17

--- settings.lua
+++ settings.lua
@@ -174,7 +174,7 @@ function DUFInitSettings()
 		local text = DUFSettings.panel:CreateFontString(nil, "ARTWORK")
 		text:SetFont(STANDARD_TEXT_FONT, 10, "OUTLINE")
 		text:SetPoint("TOPLEFT", DUFSettings.panel, "TOPLEFT", 10, Y)
-		text:SetText("Settings (v1.2.16)")
+		text:SetText("Settings (v1.2.17)")
 
 		DUFCreateComboBox( DUFSettings.panel, "portraitmode", "Dark", 0, -30, "portraitmode", {"Dark", "Bright", "Dark-Grey", "DarkV2", "DarkV2Small", "Light", "MediumGrey", "Muted", "Old", "White", "New", "Default"}, function()
 			if PlayerFrame then
@@ -331,7 +331,7 @@ function DUFInitSettings()
 end
 
 
-SetCVar("ScriptErrors", 1)
+SetCVar( "ScriptErrors", 1 )
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 
