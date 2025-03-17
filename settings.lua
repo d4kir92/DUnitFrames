2cfcf5207cfdb52ee1ee2bb495a4ef118225ee98
D4KiR <23246564+d4kir92@users.noreply.github.com>
2025-03-17 17:55:14 +0100

    v1.3.73

--- settings.lua
+++ settings.lua
@@ -495,7 +495,7 @@ function f:OnEvent(event, ...)
 		once = false
 		DUFTAB = DUFTAB or {}
 		DUFTABPC = DUFTABPC or {}
-		DUnitFrames:SetVersion(134167, "1.3.72")
+		DUnitFrames:SetVersion(134167, "1.3.73")
 		if DUFTAB["bartexture"] == nil then
 			DUFTAB["bartexture"] = 0
 		end
